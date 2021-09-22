1aa7520458e89ce1899861577e56db36a17546a2
HugoLougo <hugo.barreiro@etudiant.univ-reims.fr>
2021-09-22 18:52:10 +0200

    simple debut pour un tictac avec 9 boutons qui ne font rien

---
+++ Jeux/tic-tac-toe.py
@@ -0,0 +1,52 @@
+from tkinter import *
+from tkinter import messagebox
+
+root = Tk()
+root.title('Tic-Tac-Toe')
+# root.iconbitmap("c:/Users/lougo/Desktop/cours/master 1/CHPS706-PROJET/Jeux/icon/tictac2.ico")
+# root.iconbitmap('./icon/tictac2.ico')
+root.iconphoto(False, PhotoImage(file='./icon/tictac.png'))
+
+
+# actions de boutons
+def b_click(b):
+    pass
+
+
+# faire des boutons cliquables
+b1 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b1))
+b2 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b2))
+b3 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b3))
+
+b4 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b4))
+b5 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b5))
+b6 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b6))
+
+b7 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b7))
+b8 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b8))
+b9 = Button(root, text="", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+            command=lambda: b_click(b9))
+
+
+b1.grid(row=0, column=0)
+b2.grid(row=0, column=1)
+b3.grid(row=0, column=2)
+
+b4.grid(row=1, column=0)
+b5.grid(row=1, column=1)
+b6.grid(row=1, column=2)
+
+b7.grid(row=2, column=0)
+b8.grid(row=2, column=1)
+b9.grid(row=2, column=2)
+
+
+root.mainloop()
