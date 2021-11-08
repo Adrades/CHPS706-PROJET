7a54c5e2f9fa54f40505cf49255fb674696067bc
Adrades <genadrades@gmail.com>
2021-11-08 19:49:45 +0100

    corrections IA (pas de lien gestionnaire)

--- IA/IA_TicTacToe.py
+++ IA/IA_TicTacToe.py
@@ -12,11 +12,10 @@ class IaTictactoe():
         self.__socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.__socket.bind(("127.0.0.1", 8081))
 
+
     def launch(self):
         while True:
-
             socket_jeu = self.__socket.recvfrom(2048)
-            
             bytes_to_send = self.random_behavior()
             self.__socket.sendto(str.encode(bytes_to_send), socket_jeu[1])
 
